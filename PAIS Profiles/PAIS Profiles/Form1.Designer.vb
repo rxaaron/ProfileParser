acb4d242abe50170c78da3345c57f4deda72602d
Aaron Taylor <aaron@rxtaylor.com>
2015-12-23 17:39:34 -0500

    Confusion, step 1

---
+++ PAIS Profiles/PAIS Profiles/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.DataSet1 = New PAIS_Profiles.DataSet1()
+        Me.datataBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.datataBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(95, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ReportViewer1
+        '
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.datataBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "PAIS_Profiles.Report1.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(13, 53)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(1315, 564)
+        Me.ReportViewer1.TabIndex = 2
+        '
+        'DataSet1
+        '
+        Me.DataSet1.DataSetName = "DataSet1"
+        Me.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'datataBindingSource
+        '
+        Me.datataBindingSource.DataMember = "datata"
+        Me.datataBindingSource.DataSource = Me.DataSet1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1340, 629)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.datataBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents datataBindingSource As BindingSource
+    Friend WithEvents DataSet1 As DataSet1
+End Class
---
+++ PAIS Profiles/PAIS Profiles/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic.FileIO.TextFieldParser
+
+Public Class Form1
+
+    Dim dt As New DataTable
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using afile As New FileIO.TextFieldParser("C:\PAISPROFILES.CSV")
+            afile.TextFieldType = FileIO.FieldType.Delimited
+            afile.HasFieldsEnclosedInQuotes = True
+            afile.SetDelimiters(",")
+            Dim currentrow As String()
+            Dim i As Integer = 0
+            While Not afile.EndOfData
+                Try
+                    currentrow = afile.ReadFields()
+                    If i = 0 Then
+                        For Each currentfield As String In currentrow
+                            dt.Columns.Add(currentfield)
+                        Next
+                    Else
+                        Dim q As Integer = 0
+                        Dim dr As DataRow = dt.NewRow()
+                        For Each currentfields As String In currentrow
+
+                            dr(q) = currentfields
+
+                            q = q + 1
+                        Next
+                        dt.Rows.Add(dr)
+                    End If
+                Catch ex As Exception
+
+                End Try
+                i = i + 1
+            End While
+            dt.TableName = "datata"
+            DataSet1.Tables.Add(dt)
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ReportViewer1.RefreshReport()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
